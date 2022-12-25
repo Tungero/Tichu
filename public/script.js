fd9a26519d2788bf4fe54ae4ad3d99871d193f2a
Tungero <zadorozhnyy.roman@sspbrno.cz>
2022-12-25 16:13:40 +0100

    dd

--- public/script.js
+++ public/script.js
@@ -1,12 +1,18 @@
 const socket = io('/')
+
+/*
 const myPeer = new Peer(undefined, {
     host: "/",
     port: "403"
 })
 
 myPeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id);
 });
+*/
+
+id = 5
+socket.emit('join-room', ROOM_ID, id);
+
 
 socket.on('user-connected', userId =>{
     console.log('user connected: ' + userId)
